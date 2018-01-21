c17fbf3e05874eba5624421bbebf71a70a86f115
Michael Robards <mikerobards@icloud.com>
2018-01-20 21:49:29 -0500

    Update files

--- PixelArtProject/designs.js
+++ PixelArtProject/designs.js
@@ -1,11 +1,33 @@
-// Select color input
-// Select size input
+var color = $('#colorPicker').val();
+var erase = false;
 
-// When size is submitted by the user, call makeGrid()
-
-
-$('button').click(function() {
+// generates grid
+$('#submit').click(function() {
   makeGrid();
+});
+
+// enables erase button
+$('#erase').click(function() {
+  if (erase === true) {
+    $('#erase').css("background", "white");
+    erase = false;
+  } else {
+    $('#erase').css("background", "skyBlue");
+    erase = true;
+  }
+});
+
+// adds color to cells USING event listeners!
+$('#pixel_canvas').on('mouseover mousedown', function(evt) {
+  evt.preventDefault();
+  if (evt.which === 1) {
+    if (erase === false) {
+      color = $('#colorPicker').val();
+      $(evt.target).css("background-color", color);
+    } else {
+      $(evt.target).css("background-color", '#fff');
+    }
+  }
 });
 
 function makeGrid() {
@@ -15,17 +37,14 @@ function makeGrid() {
   var table = $('#pixel_canvas');
   var height = $('#input_height').val();
   var width = $('#input_width').val();
-  var color = $('#colorPicker').val();
-  // generates rows
-  for (var e = 0; e < height; e++) {
+  // generates rows with while loop
+  var e = 0;
+  while (e < height) {
     table.prepend("<tr></tr>");
+    e++;
   }
-  // generates cells
+  // generates cells with for loop
   for (var i = 0; i < width; i++) {
     $('tr').prepend("<td></td>");
   }
-  // adds color when clicked
-  $('td').on('click', function() {
-    $(this).css("background-color", color);
-  });
 }
